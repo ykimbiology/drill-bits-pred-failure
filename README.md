8fb39fff17485d8784de81a9abdfc61de14a9cf0
life <life@master.(none)>
2015-09-19 08:29:39 -0400

    Organized DrillBits obj.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # drill-bits-pred-failure
+
+# Overview
+This example is based on a work by Baruah et al. 2005, which uses HMM to carry out both diagnostics and prognostics for drill bits.
+
+In the example, sensor data of the drill bits are simulated.
+
+# Reference
+Baruah R, Chinnam R.
+"HMMs for diagnostics and prognostics in machining processes"
+International Journal of Production Research. 2005. Vol 43, Issue 6. pg 1275-1293.
+[1] (http://www.tandfonline.com/doi/abs/10.1080/00207540412331327727)
--- drill-bits.py
+++ drill-bits.py
@@ -1,4 +1,15 @@
 
+"""
+This example is based on the following paper:
+###
+
+
+1) Generate simulated drill-bits measurement data.
+2) Run cross-validations to detemrine optimal number of hidden states.
+3) Plot time vs drill-bits measurement data for N drills.
+ 
+"""
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,17 +17,22 @@ from hmmlearn.hmm import GaussianHMM
 
 class DrillBit(object):
   """
-  Generates simulated drill-bit usage data: thrust and torque.
+  Generates simulated drill-bit thrust and torque data.
   """
   def __init__(self,):
     # Keeps track of how many times the drill-bit has been used:
     self.usage_count = 0
-    self.x = np.linspace(0.1, 4, 5000)
+    self.x = np.linspace(0.1, 4, 1000)
     self.k = -5
+    self.j = 0
+    self.thrust = []
+    self.torque = []
     #k = np.exp(-2)
 
   def update(self):
-    self.k = self.k + 3
+    #self.k = self.k + 3
+    self.k = self.k + 1.2
+    self.j = self.j + 0.1
 
   def get_kfactor(self):
     kfactor = np.exp(self.k)
@@ -29,21 +45,40 @@ class DrillBit(object):
   def get_thrust(self):
     x = self.x
     k = self.get_kfactor()
+    j = self.j
     mu = 0.0 
     #print 'mu=',mu
-    sigma = 0.5 + k
+    sigma = 0.5 + j
+    #sigma = 0.5
     pdfa = (np.exp(-(np.log(x) - mu)**2 / (2 * sigma**2)) / (x * sigma * np.sqrt(2 * np.pi)))
-    return pdfa
+    pdfa = (1.0 + k)*pdfa
+    xmax = np.max(pdfa)
+    v = np.array([0.1*random.random()*xmax for i in range(len(pdfa))])
+    pdfa = pdfa + v
+    pdfa = list(pdfa)
+    pdfa.reverse()
+    self.thrust.extend(pdfa)
+    return np.array(pdfa)
 
 
   def get_torque(self):
     x = self.x
     k = self.get_kfactor()
+    j = self.j
+    print 'k=',k
     mu = 0.5 
     #print 'mu=',mu
-    sigma = 0.5 + 2*k
+    sigma = 0.5 + j
+    #sigma = 0.5
     pdfb = (np.exp(-(np.log(x) - mu)**2 / (2 * sigma**2)) / (x * sigma * np.sqrt(2 * np.pi)))
-    return pdfb
+    pdfb = (1.0+2*k)*pdfb
+    xmax = np.max(pdfb)
+    v = np.array([0.1*random.random()*xmax for i in range(len(pdfb))])
+    pdfb = pdfb + v
+    pdfb = list(pdfb)
+    pdfb.reverse()
+    self.torque.extend(pdfb)
+    return np.array(pdfb)
 
   def drill(self):
     """
@@ -55,9 +90,19 @@ class DrillBit(object):
     xdata = (pdf_thrust, pdf_torque)
     return xdata
 
+def plot_thrust_vs_torque():
+  pass
+
+def plot_time_vs_x(): 
+  pass
+
 if __name__ == '__main__':
   da = DrillBit()
   xt = da.get_time()
+  (xthrust, xtorque) = da.drill()
+  (xthrust, xtorque) = da.drill()
+  (xthrust, xtorque) = da.drill()
+  (xthrust, xtorque) = da.drill()
   (xthrust, xtorque) = da.drill()
   (xthrust, xtorque) = da.drill()
   
@@ -65,8 +110,8 @@ if __name__ == '__main__':
 
   # Train HMM!
   # make an HMM instance and execute fit
-  #model = GaussianHMM(n_components=5, covariance_type="diag", n_iter=1000).fit(X)
-  model = GaussianHMM(n_components=10, covariance_type="diag", n_iter=1000).fit(X)
+  model = GaussianHMM(n_components=5, covariance_type="diag", n_iter=1000).fit(X)
+  #model = GaussianHMM(n_components=10, covariance_type="diag", n_iter=1000).fit(X)
 
   # predict the optimal sequence of internal hidden state
   hidden_states = model.predict(X)
